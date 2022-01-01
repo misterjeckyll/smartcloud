0fbf8b98fffe1f40d0e9662e23d09c34b8879a3f
william pantry <william.pantry@atos.net>
2022-01-01 15:15:03 +0100

    folders

---
+++ smartcloud.ino
@@ -0,0 +1,52 @@
+#include <NeoPixelBus.h>
+#include <NeoPixelAnimator.h>
+#include <WiFiUdp.h>
+#include <WiFiClient.h>
+#include <EEPROM.h>
+#include <ESP8266WiFi.h>
+#include <DNSServer.h>
+#include <ESP8266mDNS.h>
+#include <ArduinoOTA.h>
+#include <ESP8266WebServer.h>
+#include <FS.h>                // SPIFFS management
+#include "header/cloud.h"
+
+bool serverMode;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("Cloud initialisation...");
+  setup_neopix();
+  blinkRGB(RgbColor(100,100,200), 500, 125);
+  initEEPROM();
+  
+  if (connectWifi()) {
+    Serial.print("Connected to wifi ");
+    Serial.print(WiFi.SSID());
+    Serial.print(" by ");
+    Serial.println(WiFi.hostname());
+    serverMode = false;
+    Serial.print("At ");
+    Serial.println(WiFi.localIP());
+    blinkRGB(RgbColor(0,100,0), 1000, 125);
+    multicastDnsInit();
+  }else{
+    serverMode = true;
+    accessPointInit();
+    captiveDnsInit();
+  }
+  serverInit();
+  connectOTA();
+}
+
+void loop() {
+  serverUpdate();
+  OTAUpdate();
+  if(serverMode){
+    captiveDnsUpdate();
+  }else{
+    multicastDnsUpdate();
+  }
+  commands_loop();
+}
